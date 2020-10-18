fcaea0c1ca286fbae6dde99e76dd5056ca76e27d
Evictory <emerson_vitorio@live.com>
2020-10-18 19:47:35 -0300

    Getting form data

--- src/view/LetYourContact/index.tsx
+++ src/view/LetYourContact/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
+import { useState } from 'react';
 
 import NavButton from '../../components/NavButton';
 import Title from '../../components/Title';
@@ -9,19 +10,40 @@ import { Container, LabelText, Button, ButtonText } from './styles';
 
 const LetYourContact: React.FC = () => {
   const navigation = useNavigation();
+  const [name, onChangeName] = useState('');
+  const [email, onChangeEmail] = useState('');
+  const [contact, onChangeContact] = useState('');
   const sendData = () => {
-    console.log('here!');
+    let data = {
+      name,
+      email,
+      contact,
+    };
+    console.log(data);
   };
+
   return (
     <>
       <Container>
         <Title>Deixe seu contato!</Title>
         <LabelText>Nome completo</LabelText>
-        <Input placeholder="Insira seu nome aqui" />
+        <Input
+          onChangeText={(text) => onChangeName(text)}
+          value={name}
+          placeholder="Insira seu nome aqui"
+        />
         <LabelText>E-mail</LabelText>
-        <Input placeholder="Seu e-mail aqui :)" />
+        <Input
+          onChangeText={(text) => onChangeEmail(text)}
+          value={email}
+          placeholder="Seu e-mail aqui :)"
+        />
         <LabelText>Número para contato</LabelText>
-        <Input placeholder="Pode ser o celular ou fixo ;)" />
+        <Input
+          onChangeText={(text) => onChangeContact(text)}
+          value={contact}
+          placeholder="Pode ser o celular ou fixo ;)"
+        />
         <Button onPress={() => sendData()}>
           <ButtonText>Enviar dados</ButtonText>
         </Button>
